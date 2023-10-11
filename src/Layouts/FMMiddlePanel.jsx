8290848c1cbe12abbad60cdc396e35d21ddef72e
AndrijaDev <91664827+AndrijaWBDev@users.noreply.github.com>
2023-10-11 20:56:27 +0900

    add menu

--- src/Layouts/FMMiddlePanel.jsx
+++ src/Layouts/FMMiddlePanel.jsx
@@ -166,27 +166,27 @@ function NameCell(props) {
         }}
       >
         <div className="flex flex-col gap-1 text-sm font-medium">
-          <div className="flex items-center px-5 py-1 gap-2 hover:bg-[#1976d214]">
+          {/* <div className="flex items-center px-5 py-1 gap-2 hover:bg-[#1976d214]">
             <AiOutlineDownload />
             Download
           </div>
           <div className="flex items-center px-5 py-1 gap-2 hover:bg-[#1976d214]">
             <MdOutlineDriveFileRenameOutline />
             Rename
-          </div>
+          </div> */}
           <div className="flex items-center px-5 py-1 gap-2 hover:bg-[#1976d214]">
             <BiPurchaseTagAlt />
             Tag
           </div>
-          <div className="flex items-center px-5 py-1 gap-2 hover:bg-[#1976d214] border-b-[1px] border-[#DEE0E4] ">
+          {/* <div className="flex items-center px-5 py-1 gap-2 hover:bg-[#1976d214] border-b-[1px] border-[#DEE0E4] ">
             <MdOutlineContentCopy />
             Copy
-          </div>
+          </div> */}
           <div className="flex items-center px-5 py-1 gap-2 hover:bg-[#1976d214]">
             <LuStickyNote />
             Add Note
           </div>
-          <div className="flex items-center px-5 py-1 gap-2 hover:bg-[#1976d214]">
+          {/* <div className="flex items-center px-5 py-1 gap-2 hover:bg-[#1976d214]">
             <RiUserSharedLine />
             Share
           </div>
@@ -197,11 +197,11 @@ function NameCell(props) {
           <div className="flex items-center px-5 py-1 gap-2 hover:bg-[#1976d214] border-b-[1px] border-[#DEE0E4] ">
             <GrCircleInformation />
             File information
-          </div>
-          <div className="flex items-center px-5 py-1 gap-2 hover:bg-[#1976d214] ">
+          </div> */}
+          {/* <div className="flex items-center px-5 py-1 gap-2 hover:bg-[#1976d214] ">
             <TfiTrash />
             Delete
-          </div>
+          </div> */}
         </div>
       </Popup>
     </div>
