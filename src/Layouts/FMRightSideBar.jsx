dd54e7f76c8e0965e42c22bb598a469a86cad0c9
AndrijaDev <91664827+AndrijaWBDev@users.noreply.github.com>
2023-10-17 02:36:08 +0900

    update right scroll

--- src/Layouts/FMRightSideBar.jsx
+++ src/Layouts/FMRightSideBar.jsx
@@ -36,7 +36,9 @@ const FMRightSideBar = () => {
 
   return (
     <div
-      className="flex flex-col  px-2 py-0  rightsidebar h-[calc(100vh-160px)]"
+      className={`absolute left-0 flex flex-col  px-2 py-0  rightsidebar h-[calc(100vh-160px)] 
+      ${activeTab !== "members" ? "w-full" : "w-[calc(100% - 5px)]"}
+      `}
       ref={containerRef}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
